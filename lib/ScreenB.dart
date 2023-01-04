af58c0953ce586e0a0f492c48f9513151ba2ced6
kwonseeun <0115km@naver.com>
2023-01-04 23:15:28 +0900

    Commit 확인용

---
+++ lib/ScreenB.dart
@@ -0,0 +1,16 @@
+class ScreenB extends StatefulWidget {
+
+  _ScreenBState createState() => _ScreenBState();
+}
+
+class _ScreenBState extends State<ScreenB> {
+
+  @override
+  void initState() {
+    super.initState();
+}
+
+Widget build(BuildContext context) {
+
+  }
+}
